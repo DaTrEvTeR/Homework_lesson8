63ae9c665562f108badadf9bea534992e3f869cd
Miho <rozhkovm176@gmail.com>
2023-07-29 18:13:53 +0300

    Task 1 done

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 # Написати рекурсивну функцію знаходження ступеня числа.
 
 
-###
+def my_pow(user_number: int, user_pow: int) -> int:
+    if user_pow < 0:
+        raise Exception('Enter only positive pow please!')
+    if user_pow == 0:
+        return 1
+    if user_pow == 1:
+        return user_number
+    return user_number * my_pow(user_number, user_pow - 1)
+
+
+try:
+    user_number = int(input('Enter a number: '))
+    user_pow = int(input('Enter a pow: '))
+    print(my_pow(user_number, user_pow))
+except Exception as error:
+    print('Oops... some problem here, please try again')
 
 
 # Завдання 2.
